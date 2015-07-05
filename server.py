cc602850b683e3d9c014091f76e6d5d5f50fafde
Nate Morse <morse.nate@gmail.com>
2015-07-05 10:06:36 -0400

    proof of concept

---
+++ server.py
@@ -0,0 +1,32 @@
+from websocket_server import WebsocketServer
+
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+	print("New client connected and was given id %d" % client['id'])
+	server.send_message_to_all('{"msg":"Hey all, a new client has joined us"}')
+
+
+# Called for every client disconnecting
+def client_left(client, server):
+	print("Client(%d) disconnected" % client['id'])
+
+
+# Called when a client sends a message
+def message_received(client, server, message):
+	if len(message) > 200:
+		message = message[:200]+'..'
+	if message == 'start-stream':
+		# echo start-streaming
+		server.send_message_to_all('{"msg": "Starting to stream"}')
+		# start the streaming
+		server.send_message_to_all('{"data": [30, 47, 41, 86, 13, 73, 89, 12]}')
+
+	print("Client(%d) said: %s" % (client['id'], message))
+
+
+PORT=9001
+server = WebsocketServer(PORT)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()
